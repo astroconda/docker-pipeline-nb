733eaa196f13923912851d0be0b24614da0102df
Joseph Hunkeler <jhunkeler@gmail.com>
2019-03-04 10:37:43 -0500

    Handle notebook issue #4399

--- etc/pkgs/001-jupyter.sh
+++ etc/pkgs/001-jupyter.sh
@@ -6,6 +6,10 @@ pip install --upgrade --progress-bar=off \
     jupyterhub==0.9.4 \
     jupyterlab==0.35.4
 
+# Workaround for kernel+WebSocket failure
+# See: https://github.com/jupyter/notebook/issues/4399
+pip install --progress-bar=off  tornado==5.1.1
+
 jupyter labextension install @jupyterlab/hub-extension@^0.12.0
 jupyter notebook --generate-config -y
 
